fbf10c6299f63b39f59c54ef4128b55b7a0ab612
Mauricio Duran <mduran@ammonrizos.com.mx>
2020-02-27 17:58:29 -0600

    Changed file to import react-cookie and be able to use cookies for different purposes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router , Switch, Route } from "react-router-dom";
+import {withCookies} from 'react-cookie';
+
 import Navigation from './components/Navigation';
 import Home from './components/Home';
 import Directory from './components/Directory';
@@ -7,20 +9,55 @@ import Calendar from './components/Calendar';
 import Crosses from './components/Crosses';
 import Diagrams from './components/Diagrams';
 import ToggleNavButton from './components/ToggleNavButton.js';
+import LoginForm from './components/LoginForm';
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
+
+    state = {
+            isLogged: false,
+            user: {},
+            loginModalShown: false,
+        }
+       
+    handleModalHide = (modalId) => {
+        switch(modalId){
+            case "loginModal": this.setState({loginModalShown: false}); break;
+            default: break;
+        }
+    }
+
+    handleModalShow = (modalId) => {
+        switch(modalId){
+            case "loginModal": this.setState({loginModalShown: true}); break;
+            default: break;
+        }
+    }
+    
+    componentDidMount(){
+        const {cookies} = this.props;
+        const name = cookies.get('name');
+        if(name){
+            alert(name);
+        } else{
+            alert("You need to log in");
+        }
+    }
+
     render() {
         return (
             <Router >
-                <Navigation />
+                <Navigation showModal={()=>this.handleModalShow("loginModal")} cookies={this.props.cookies}/>
                 <div id="content">
                     <ToggleNavButton/>
+                    <LoginForm isLogged={this.state.isLogged} show={this.state.loginModalShown} hide={()=>{this.handleModalHide("loginModal")}} />
                     <Switch>
-                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/" render={()=> {return <Home cookies={this.props.cookies}/>} }/>
                         <Route path="/directory" component={Directory}/>
                         <Route path="/calendar" component={Calendar}/>
                         <Route path="/crosses" component={Crosses}/>
-                        <Route path="/diagrams" component={Diagrams}/>
+                        <Route path="/diagrams" render={()=> {return <Diagrams cookies={this.props.cookies}/>} }/>{/*</Route>component={Diagrams}/>*/}
                     </Switch>
 
                 </div>
@@ -29,4 +66,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default withCookies(App);
