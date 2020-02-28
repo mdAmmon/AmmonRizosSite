840f830c94bcf9881e89f466c5233724f95e5132
Mauricio Duran <mduran@ammonrizos.com.mx>
2020-02-28 14:08:57 -0600

    Deleted temporary content used for testing on cookies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
-
-    setCookies = () => {
-        const { cookies } = this.props;
-        // now.setSeconds(now.getSeconds()+15);
-        const hour = 3600;
-        cookies.set('name', this.state.name, {maxAge: 3 * hour});
-        this.props.login(cookies.get('name'));
-        alert(cookies.get('name'));
+    state = {
+        name: ''
     }
+
+
     render() {
         return (
             <div>
                 <h1>Home</h1>
-                <input type="text" onChange={(e) => this.setState({name: e.target.value})} />
-                <button onClick={this.setCookies}>Get them Cookies!</button>
             </div>
 
         );
