ee1792e44a5d972d0a478b4ee9b645426f4fa992
Mauricio Duran <mduran@ammonrizos.com.mx>
2020-02-28 10:58:13 -0600

    Created function to change state when there is a login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router , Switch, Route } from "react-router-dom";
-import {withCookies} from 'react-cookie';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { withCookies } from 'react-cookie';
 
 import Navigation from './components/Navigation';
 import Home from './components/Home';
@@ -16,48 +16,63 @@ import "bootstrap/dist/css/bootstrap.min.css";
 class App extends Component {
 
     state = {
-            isLogged: false,
-            user: {},
-            loginModalShown: false,
+        isLogged: false,
+        user: {},
+        loginModalShown: false,
+    }
+
+    componentDidMount() {
+        const { cookies } = this.props;
+        const name = cookies.get('name');
+        if (name) {
+            this.setState({ isLogged: true });
         }
-       
+    }
+
     handleModalHide = (modalId) => {
-        switch(modalId){
-            case "loginModal": this.setState({loginModalShown: false}); break;
+        switch (modalId) {
+            case "loginModal": this.setState({ loginModalShown: false }); break;
             default: break;
         }
     }
 
     handleModalShow = (modalId) => {
-        switch(modalId){
-            case "loginModal": this.setState({loginModalShown: true}); break;
+        switch (modalId) {
+            case "loginModal": this.setState({ loginModalShown: true }); break;
             default: break;
         }
     }
-    
-    componentDidMount(){
+
+    logout = () =>{
         const {cookies} = this.props;
-        const name = cookies.get('name');
-        if(name){
-            alert(name);
-        } else{
-            alert("You need to log in");
-        }
+
+        cookies.remove('name');
+
+        this.setState({isLogged: false});
+
+        return this.state.isLogged;
+
+    }
+
+    login = (name) =>{
+        const {cookies} = this.props;
+        this.setState({isLogged: true, user: {name}});
+
     }
 
     render() {
         return (
             <Router >
-                <Navigation showModal={()=>this.handleModalShow("loginModal")} cookies={this.props.cookies}/>
+                <Navigation showModal={() => this.handleModalShow("loginModal")} logout={this.logout} isLogged={this.state.isLogged} />
                 <div id="content">
-                    <ToggleNavButton/>
-                    <LoginForm isLogged={this.state.isLogged} show={this.state.loginModalShown} hide={()=>{this.handleModalHide("loginModal")}} />
+                    <ToggleNavButton />
+                    <LoginForm isLogged={this.state.isLogged} show={this.state.loginModalShow} hide={() => { this.handleModalHide("loginModal") }} />
                     <Switch>
-                        <Route exact path="/" render={()=> {return <Home cookies={this.props.cookies}/>} }/>
-                        <Route path="/directory" component={Directory}/>
-                        <Route path="/calendar" component={Calendar}/>
-                        <Route path="/crosses" component={Crosses}/>
-                        <Route path="/diagrams" render={()=> {return <Diagrams cookies={this.props.cookies}/>} }/>{/*</Route>component={Diagrams}/>*/}
+                        <Route exact path="/" render={() => { return <Home cookies={this.props.cookies} login={this.login}/> }} />
+                        <Route path="/directory" component={Directory} />
+                        <Route path="/calendar" component={Calendar} />
+                        <Route path="/crosses" component={Crosses} />
+                        <Route path="/diagrams" render={() => { return <Diagrams cookies={this.props.cookies} /> }} />{/*</Route>component={Diagrams}/>*/}
                     </Switch>
 
                 </div>
