10867ed9da2929ab712094aaae28ae6de8011b63
Mauricio Duran <mduran@ammonrizos.com.mx>
2020-03-04 15:48:40 -0600

    Modified the SearchBar component to be able to have extra content within.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import "../styles/searchbar.css"
 class SearchBar extends Component {
     render() {
         return (
             <div className="col-md-8 col-10 searchbar">
-                <input type="text" className="form-control" autoComplete="off" name="name" id="searchEmployee"
-                    placeholder={this.props.placeholder} onKeyUp={this.props.onKeyUp}/>
+                <input type="text" className="form-control" autoComplete="off" name="name" id={this.props.id}
+                    placeholder={this.props.placeholder} onKeyUp={this.props.onKeyUp} />
+                {this.props.children}
             </div>
         );
 
