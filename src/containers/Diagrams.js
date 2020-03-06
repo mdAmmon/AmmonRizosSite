cf2295ececde0e4467d76dcb62618a3cd3b56e8f
Mauricio Duran <mduran@ammonrizos.com.mx>
2020-03-06 16:50:31 -0600

    Deleted testing code from file

--- src/containers/Diagrams.js
+++ src/containers/Diagrams.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 
 class Diagrams extends Component {
-    setCookies = () => {
-        const { cookies } = this.props;
-
-        alert(cookies.get('name'));
-    }
 
     render() {
         return (
             <div>
                 <h1>Diagrams</h1>
-                <button onClick={this.setCookies}>Get them Cookies!</button>
             </div>
         );
 
