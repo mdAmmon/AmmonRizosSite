365acbd983921e625512c778f80dda06d336f53f
Mauricio Duran <mduran@ammonrizos.com.mx>
2020-03-31 14:43:14 -0600

    Included BoxTitle and Weather Widget on Dashboard

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BoxTitle from '../components/DashBoard/BoxTitle';
+import '../styles/dashboard.css';
 
 class Home extends Component {
     state = {
@@ -13,10 +14,19 @@ class Home extends Component {
 
     render() {
         return (
-            <div>
-                <BoxTitle>
-                    <h2>Adios Mundo</h2>
-                </BoxTitle>
+            <div style={{ height: "100vh" }}>
+                <BoxTitle id="welcoming" title="Hello There" imageUrl="./img/ar.jpeg" />
+
+                <div className="shadowing roundEdges dashboardBox" id="weather">
+                    <a className="weatherwidget-io" href="https://forecast7.com/es/20d67n103d42/zapopan/"
+                        data-label_1="Zapopan," data-label_2="Mexico" data-mode="Current" data-theme="pure">ZAPOPAN WEATHER
+                    </a>
+                </div>
+
+                
+
+
+
             </div>
 
         );
