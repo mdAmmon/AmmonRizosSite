826729e8f0641004d3d73ffa17e19edfba15ee09
Mauricio Duran <mduran@ammonrizos.com.mx>
2020-05-19 15:27:36 -0500

    set gray_square image as background for the employee cards as user image url are not available yet

--- src/components/Directory/DirectoryList.js
+++ src/components/Directory/DirectoryList.js
@@ -9,7 +9,8 @@ class DirectoryList extends Component {
                         priority={employee.priority} key={employee.email} name={employee.nombre}
                         position={employee.position} email={employee.email} phone={employee.direct_phone}
                         extension={employee.ext} mobile={employee.mobile_phone} manager={employee.Manager} 
-                        url={employee.url} location={employee.location_name}
+                        url="gray_square.png" location={employee.location_name}
+                        // url={employee.url}
                     />)
                 })}
             </div>
