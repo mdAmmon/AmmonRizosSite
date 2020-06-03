550cf77551d074f9c72b1957d39f2f99fc1d773d
Mauricio Duran <mduran@ammonrizos.com.mx>
2020-06-03 13:11:40 -0500

    Updates

---
+++ precache-manifest.0698abdaddab35c58e42bb92b6dbc326.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807309c2e826d3ff83c7d1f88585a961",
+    "url": "/AmmonRizosSite/index.html"
+  },
+  {
+    "revision": "5149e1382f8e7acf7381",
+    "url": "/AmmonRizosSite/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "da4e021ec7479a5d5d46",
+    "url": "/AmmonRizosSite/static/css/main.f87eeb37.chunk.css"
+  },
+  {
+    "revision": "5149e1382f8e7acf7381",
+    "url": "/AmmonRizosSite/static/js/2.b8489f41.chunk.js"
+  },
+  {
+    "revision": "ea8a1fe7bb9452c970f2c6b1235ee2f7",
+    "url": "/AmmonRizosSite/static/js/2.b8489f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4e021ec7479a5d5d46",
+    "url": "/AmmonRizosSite/static/js/main.8e511fa3.chunk.js"
+  },
+  {
+    "revision": "d18af6aa38384b1a76b0",
+    "url": "/AmmonRizosSite/static/js/runtime-main.75c4a95b.js"
+  },
+  {
+    "revision": "bc927d2a525d448f00f01d99aee98a3f",
+    "url": "/AmmonRizosSite/static/media/arLogo.bc927d2a.png"
+  },
+  {
+    "revision": "de95dec3752eb382a6c21d39845ba370",
+    "url": "/AmmonRizosSite/static/media/logo_On.de95dec3.png"
+  },
+  {
+    "revision": "a5688a0baa76ba4e39329e3f3edf5dbb",
+    "url": "/AmmonRizosSite/static/media/mexico-flag.a5688a0b.svg"
+  },
+  {
+    "revision": "2c35bd94daf38f8f26243c29e0809d49",
+    "url": "/AmmonRizosSite/static/media/united-states-of-america-flag.2c35bd94.svg"
+  }
+]);
