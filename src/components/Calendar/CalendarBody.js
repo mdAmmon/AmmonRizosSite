16a84f3da5b1bc530c16fc31dfb5bd1f5433a612
Mauricio Duran <mduran@ammonrizos.com.mx>
2020-06-10 14:41:47 -0500

    created function to load events + added new url for requests to the backend

--- src/components/Calendar/CalendarBody.js
+++ src/components/Calendar/CalendarBody.js
@@ -20,20 +20,28 @@ class CalendarBody extends Component {
         }
     }
 
+    componentDidMount() {
+        this.loadEvents();
+    }
+
     //Checks if current month is different from updated month and if it is, it fetches the events for the new month-year date.
     componentDidUpdate(nextProps) {
         if (nextProps.year !== this.props.year || nextProps.month !== this.props.month || nextProps.updatePage !== this.props.updatePage) {
-            let url = "https://arizoslocal.herokuapp.com/includes/calendarP.php?month=" + pad(this.props.month + 1, 2) + "&year=" + this.props.year;
-            fetch(url).then(res => res.json()).then(res => this.setState({ events: res })
-            );
+            this.loadEvents();
         }
+    }
+
+    loadEvents() {
+        // https://arizoslocal.herokuapp.com/includes/calendarP.php
+        let url = "http://localhost:3001/calendar/?month=" + pad(this.props.month + 1, 2) + "&year=" + this.props.year;
+        fetch(url).then(res => res.json()).then(res => this.setState({ events: res }));
     }
 
     getEventsforDate = (day, month, year) => {
         let str = year + "-" + pad(month + "", 2) + "-" + pad("" + day, 2);
 
         let eventsForTheMonth = this.state.events.filter(element => {
-            if (str === element.fechaInicio) return true;
+            if (str === element.fechaInicio.slice(0,10)) return true;
             if (element.fechaFin != null && element.fechaFin >= str && element.fechaInicio <= str) return true;
             return false;
         });
@@ -54,7 +62,7 @@ class CalendarBody extends Component {
 
         for (let i = 0; i < 6; i++) {
             cells = [];
-            if(day>daysInMonth(this.props.month, this.props.year)) break;
+            if (day > daysInMonth(this.props.month, this.props.year)) break;
             for (let j = 0; j < 7; j++) {
 
                 keyV = "" + i + j;
@@ -74,19 +82,19 @@ class CalendarBody extends Component {
                         classList += "today ";
                     }
                     let funcDay = day;
-                    let eventsForDate = this.getEventsforDate(funcDay, this.props.month+1, this.props.year);
+                    let eventsForDate = this.getEventsforDate(funcDay, this.props.month + 1, this.props.year);
                     innerContent = (j === 0 || j === 6) ? "" : this.props.printEvents(eventsForDate);
                     innerContent = (<>
                         <p>{day}</p>
                         {innerContent}
                     </>);
 
-                    cells.push(<td key={keyV} id={eventsForDate[1]} onClick={()=> this.props.onClickFunc(eventsForDate)} className={classList}>{innerContent}</td>);
+                    cells.push(<td key={keyV} id={eventsForDate[1]} onClick={() => this.props.onClickFunc(eventsForDate)} className={classList}>{innerContent}</td>);
                     day++;
-                } else{
+                } else {
                     cells.push(<td key={keyV}></td>);
                 }
-                
+
             }
             rowArr.push(<tr key={i}>{cells}</tr>);
         }
