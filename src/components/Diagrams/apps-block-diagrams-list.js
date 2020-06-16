0eb2a9405bed23030e96a2f5da6fccd9a002073d
Duran <duran@MBP-Duran.local>
2020-06-16 12:33:53 -0500

    Created object including application fields and subfields

---
+++ src/components/Diagrams/apps-block-diagrams-list.js
@@ -0,0 +1,21 @@
+
+const applicationFields = {
+    automotive: [
+        "Instrument Cluster",
+        "Audio and Infotainment Systems",
+        "ECU Power Management",
+        "Front View Camera",
+        "RearView Camera",
+        "Surround View Camera",
+        "Door Module",
+        "Body Electronics",
+        "Automotive HVAC",
+        "Front Lighting"
+    ],
+    dcdc: [],
+    motorcontrol: [],
+    ledlighting: []
+
+}
+
+export default applicationFields;
