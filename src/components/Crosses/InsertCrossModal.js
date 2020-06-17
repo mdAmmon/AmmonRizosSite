def1c0f333f96579802b547389f57b1d1a9639fc
Duran <duran@MBP-Duran.local>
2020-06-17 14:10:46 -0500

    Added new functionality for inserting new competitors

--- src/components/Crosses/InsertCrossModal.js
+++ src/components/Crosses/InsertCrossModal.js
@@ -17,17 +17,13 @@ class InsertCrossModal extends React.Component {
         principalPart: "",
         direct: "YES",
         comments: "",
+        addNewCompetitor: false,
+        newCompetitor: ""
     }
 
     componentDidMount() {
-        let comps, mfgs;
-        // https://arizoslocal.herokuapp.com/includes/getCompetitors.php
-        fetch("https://arback-node.herokuapp.com/competitors")
-            .then(res => res.json())
-            .then(res => {
-                comps = res;
-                this.setState({ competitors: comps, selectedCompetitor: comps[0].competitor_id });
-            });
+        let mfgs;
+
         // https://arizoslocal.herokuapp.com/includes/getPrincipals.php
         fetch("https://arback-node.herokuapp.com/principals")
             .then(res => res.json())
@@ -36,6 +32,53 @@ class InsertCrossModal extends React.Component {
                 this.setState({ manufacturers: mfgs, selectedPrincipal: mfgs[0].brand_id });
             });
 
+        this.fetchCompetitors();
+
+    }
+
+    fetchCompetitors = () => {
+        let comps;
+
+        // https://arizoslocal.herokuapp.com/includes/getCompetitors.php
+        fetch("https://arback-node.herokuapp.com/competitors")
+            .then(res => res.json())
+            .then(res => {
+                comps = res;
+                this.setState({ competitors: comps, selectedCompetitor: comps[0].competitor_id });
+            });
+    }
+
+    addNewCompetitor = () => {
+        let body = {};
+        body.competitor = this.state.newCompetitor.toUpperCase();
+
+        fetch("https://arback-node.herokuapp.com/competitors", {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(body)
+
+        })
+            .then(res => res.json())
+            .catch(error => alert("something went wrong. :("))
+            .then(response => {
+                console.log(response);
+                this.toggleAddCompetitorButton();
+                this.fetchCompetitors();
+            });
+    }
+
+    
+
+    toggleAddCompetitorButton = () => {
+        const isAddingNewComp = this.state.addNewCompetitor;
+
+        let obj = {};
+        obj.addNewCompetitor = !isAddingNewComp;
+
+        if (isAddingNewComp) obj.newCompetitor = "";
+        this.setState(obj);
     }
 
     recordChange = (e) => {
@@ -63,7 +106,7 @@ class InsertCrossModal extends React.Component {
             return;
         }
 
-        const {selectedCompetitor, competitorPart, selectedPrincipal, principalPart, direct, comments} = this.state;
+        const { selectedCompetitor, competitorPart, selectedPrincipal, principalPart, direct, comments } = this.state;
 
         let body = {
             comp_id: selectedCompetitor,
@@ -77,14 +120,6 @@ class InsertCrossModal extends React.Component {
         console.log(JSON.stringify(body));
         // let body = new FormData();
 
-        // body.append('comp_id', this.state.selectedCompetitor);
-        // body.append('comp_model', this.state.competitorPart);
-        // body.append('brnd_id', this.state.selectedPrincipal);
-        // body.append('brnd_model', this.state.principalPart);
-        // body.append('direct', this.state.direct);
-        // body.append('comments', this.state.comments);
-
-        // "https://arizoslocal.herokuapp.com/includes/insertCrossP.php"
         fetch("https://arback-node.herokuapp.com/crosses", {
             headers: {
                 'Content-Type': 'application/json'
@@ -95,17 +130,16 @@ class InsertCrossModal extends React.Component {
         })
             .then(res => res.json())
             .catch(error => alert("something went wrong. :("))
-            .then(response =>{
-                if(response.success){
+            .then(response => {
+                if (response.success) {
                     alert(response.success);
                     this.props.updateTable();
-                } else if(response.error){
+                } else if (response.error) {
                     alert(response.error);
                 }
 
                 this.props.hide();
             });
-
     }
 
     render() {
@@ -122,22 +156,54 @@ class InsertCrossModal extends React.Component {
                     <ModalTitle>Insert Cross</ModalTitle>
                 </ModalHeader>
 
-                <ModalBody>
+                <ModalBody style={{ transition: 'all 0.5s ease-out' }}>
                     <div className="form-group">
-                        <label htmlFor="compSelect">Competitor:</label>
 
-                        <select name="competitor" className="custom-select" id="selectedCompetitor" onChange={this.recordChange}>
-                            {this.state.competitors.map(
-                                element => <option key={element.competitor_id} value={element.competitor_id}>{element.competitor_name}</option>
-                            )}
-                        </select>
+                        <div className='row form-group'>
+
+                            <div className='col-12'>
+                                <label htmlFor="compSelect">Competitor:</label>
+                                <select name="competitor" className="custom-select col-10" style={{ maxWidth: "88%" }} id="selectedCompetitor" onChange={this.recordChange}>
+                                    {this.state.competitors.map(
+                                        element => <option key={element.competitor_id} value={element.competitor_id}>{element.competitor_name}</option>
+                                    )}
+                                </select>
+
+                                <button className='form-control' style={{ float: "right", width: "10%" }} onClick={this.toggleAddCompetitorButton}>
+                                    {this.state.addNewCompetitor ? <i className="fa fa-minus red" aria-hidden="true"></i> : <i className="fa fa-plus green" aria-hidden="true"></i>}
+                                </button>
+                            </div>
+                        </div>
                     </div>
+
+                    {
+                        this.state.addNewCompetitor ?
+                            (<div className='row form-group'>
+                                <div className='col-12'>
+                                    <input type="text" name="compName" id="newCompetitor" className="form-control form-group"
+                                        onChange={this.recordChange} onBlur={this.validateForm}
+                                        style={{ float: "left", width: "78%" }}
+                                        placeholder="New Competitor's name" aria-describedby="newCompetitor" />
+
+                                    {this.state.newCompetitor ? (<button className="btn btn-success form-group" id="addComp"
+                                        style={{ width: "20%", float: "right" }}
+                                        onClick={this.addNewCompetitor}>
+                                        Add
+                                    </button>) :
+                                        (<button className="btn btn-success form-group" disabled
+                                            style={{ width: "20%", float: "right" }}>
+                                            Add
+                                    </button>)}
+
+                                </div>
+                            </div>) : ""
+                    }
 
                     <div className="row form-group">
                         <div className="col-12">
                             <input type="text" name="nombreR" id="competitorPart" className="form-control"
                                 onKeyUp={this.recordChange} onBlur={this.validateForm}
-                                placeholder="Competitor's Part Number..." aria-describedby="helpId" />
+                                placeholder="Competitor's Part Number..." aria-describedby="competitorPartNumber" />
                         </div>
 
                     </div>
@@ -179,7 +245,7 @@ class InsertCrossModal extends React.Component {
                             id="sendReg">Send</button>}
 
                 </ModalBody>
-            </Modal>
+            </Modal >
         );
     }
 
