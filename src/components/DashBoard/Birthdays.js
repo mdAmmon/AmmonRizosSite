a438997badbab5876eb1b22eef0047fe2b86eac4
Duran <duran@MBP-Duran.local>
2020-07-13 14:44:47 -0500

    Added bbirthdays coming up functionality

--- src/components/DashBoard/Birthdays.js
+++ src/components/DashBoard/Birthdays.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 import '../../styles/birthdays.css'
-import months from '../../assets/months'
 import EmpBirthday from './EmpBirthday';
+import { pad } from '../../assets/functions';
+
+
+
+const binarySearch = (arr, value, start, end, field) => {
+    if (start > end) return start;
+
+    let mid = start + Math.floor((end - start) / 2);
+
+    if (arr[mid][field] === value) return mid;
+    else if (arr[mid][field] > value) return binarySearch(arr, value, start, mid - 1, field);
+    else return binarySearch(arr, value, mid + 1, end, field);
+}
+
+const getUpcomingBirthdays = (bdays) => {
+    // return bdays.slice(0, 3);
+    const myDate = new Date();
+
+    const date = `${pad(myDate.getMonth() + 1 + "")}/${pad(myDate.getDate() + "")}`;
+
+    const startIndex = binarySearch(bdays, date, 0, bdays.length - 1, 'birthday');
+
+    const upcomingBdays = bdays.slice(startIndex, startIndex + 3);
+
+    const birthdaysLeft = 3 - upcomingBdays.length;
+
+    for (let i = 0; i < birthdaysLeft; i++) {
+        upcomingBdays.push(bdays[i]);
+    }
+    return upcomingBdays;
+}
+
 
 
 //This should have state and get the three employees whose birthdays are coming up.
@@ -10,40 +41,34 @@ class Birthdays extends Component {
         super(props);
 
         this.state = {
-            closeBirthdays: [
-                {
-                    name: "Mauricio Duran",
-                    birthday: "October 21"
-                },
-                {
-                    name: "Mauricio Duran",
-                    birthday: "October 21"
-                },
-                {
-                    name: "Mauricio Duran",
-                    birthday: "October 21"
-                }
-            ]
+            upcomingBirthdays: []
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
+        fetch('http://localhost:3001/directory/birthdays')
+            .then( res => res.json())
+            .then( res => {
+                const birthdaysComingUp = getUpcomingBirthdays(res);
 
+                this.setState({upcomingBirthdays: birthdaysComingUp})
+            })
+            .catch( err => console.log(err));
     }
 
     render() {
-        const closeBirthdays = this.state.closeBirthdays;
+        const upcomingBirthdays = this.state.upcomingBirthdays;
         return (
             <div id={this.props.id} className="shadowing roundEdges birthdays">
                 <h6>Upcoming Birthdays</h6>
 
                 {
-                    closeBirthdays.map((el, i) => {
-                        return <EmpBirthday 
+                    upcomingBirthdays.map((el, i) => {
+                        return <EmpBirthday
                             key={i}
-                            name = {el.name}
-                            birthday = {el.birthday}
-                            url = "gray_square.png"
+                            name={el.nombre}
+                            birthday={el.birthday}
+                            url="gray_square.png"
                         />
                     })
                 }
--- src/components/DashBoard/EmpBirthday.js
+++ src/components/DashBoard/EmpBirthday.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
+import months from '../../assets/months';
 
 class EmpBirthday extends Component {
     render() {
         const img = require(`../../img/${this.props.url}`);
 
+        const date = this.props.birthday.split('/');
+        const month = months[ parseInt(date[0])-1 ];
+
         return (
             <div className="employee">
-                <div className="profilePic" style={{backgroundImage: `url(${img})`}}></div>
+                <div className="profilePic" style={{ backgroundImage: `url(${img})` }}></div>
                 <h5>{this.props.name}</h5>
-                <p>{this.props.birthday}</p>
+                <p>{month} {date[1]}</p>
             </div>
         )
     }
